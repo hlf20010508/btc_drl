f4e180f20288d6459997cc9a6d04b2c11e230a91
L-ING <hlf01@icloud.com>
2024-11-19 17:05:19 +0800

    refactor: divide read data into load and preprocess

--- dataset.py
+++ dataset.py
@@ -6,7 +6,8 @@ from okx_data import download_data
 
 class BTCDataset(Dataset):
     def __init__(self, seq_len: int, interval: str = "1H", start: str = "2020_01_01"):
-        self.data = self._read_data(interval, start)
+        self.data = self._load_data(interval, start)
+        self._preprocess_data()
         self.seq_len = seq_len
 
     def __len__(self):
@@ -20,23 +21,26 @@ class BTCDataset(Dataset):
             y, dtype=torch.float32
         )
 
-    def _read_data(interval: str = "1H", start: str = "2020_01_01"):
+    def _load_data(self, interval: str = "1H", start: str = "2020_01_01"):
         download_data(interval, start)
 
-        data = pd.read_csv(f"data/btcusdt_{interval}_{start}.csv")
+        return pd.read_csv(f"data/btcusdt_{interval}_{start}.csv")
 
-        data["Date"] = pd.to_datetime(data["Date"])
-        data.set_index("Date", inplace=True)
-        data = data.sort_index()
+    def _preprocess_data(self):
+        self.data["Date"] = pd.to_datetime(self.data["Date"])
+        self.data.set_index("Date", inplace=True)
+        self.data = self.data.sort_index()
 
-        data.fillna(method="ffill", inplace=True)
+        self.data.fillna(method="ffill", inplace=True)
 
-        price_highest = data["High"].max()
-        price_lowest = data["Low"].min()
+        price_highest = self.data["High"].max()
+        price_lowest = self.data["Low"].min()
 
         def min_max_unify_scaler(keys: list[str]):
             for key in keys:
-                data[key] = (data[key] - price_lowest) / (price_highest - price_lowest)
+                self.data[key] = (self.data[key] - price_lowest) / (
+                    price_highest - price_lowest
+                )
 
         min_max_unify_scaler(
             [
@@ -60,25 +64,25 @@ class BTCDataset(Dataset):
 
         def min_max_scaler(keys: list[str]):
             for key in keys:
-                data[key] = (data[key] - data[key].min()) / (
-                    data[key].max() - data[key].min()
+                self.data[key] = (self.data[key] - self.data[key].min()) / (
+                    self.data[key].max() - self.data[key].min()
                 )
 
         min_max_scaler(["Volume", "OBV"])
 
         def linear_scaler(keys: list[str], rate: float = 100):
             for key in keys:
-                data[key] = data[key] / rate
+                self.data[key] = self.data[key] / rate
 
         linear_scaler(["RSI7", "RSI14", "RSI21", "KDJ-K", "KDJ-D", "KDJ-J"])
 
         def standard_scaler(keys: list[str]):
             for key in keys:
-                data[key] = (data[key] - data[key].mean()) / data[key].std()
+                self.data[key] = (self.data[key] - self.data[key].mean()) / self.data[
+                    key
+                ].std()
 
         standard_scaler(["MACD", "MACD-signal", "MACD-hist"])
-
-        return data
 
     def to_dataloader(self, batch_size: int = 32, shuffle: bool = False):
         return DataLoader(self, batch_size=batch_size, shuffle=shuffle)
