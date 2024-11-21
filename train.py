3ef6a224a5b9ac68947248b5fc407837ef64ae5d
L-ING <hlf01@icloud.com>
2024-11-21 21:42:07 +0800

    feat: draw

--- train.py
+++ train.py
@@ -4,6 +4,7 @@ import os
 from model import GRUPPO
 from dataset import BTCDataset
 from trading_env import TradingEnv
+import matplotlib.pyplot as plt
 
 
 def run(
@@ -18,6 +19,7 @@ def run(
     dropout=0.2,
     learning_rate=0.001,
     gamma=0.99,
+    features=[],
 ):
     if torch.cuda.is_available():
         device = torch.device("cuda")
@@ -26,9 +28,9 @@ def run(
     else:
         device = torch.device("cpu")
 
-    dataset = BTCDataset(seq_len, interval, start)
+    dataset = BTCDataset(seq_len, interval, start, features)
     dataloader = DataLoader(dataset, batch_size=batch_size)
-    trading_env = TradingEnv(dataset.data["Close"])
+    trading_env = TradingEnv(dataset.data["Close"][dataset.seq_len :])
 
     input_dim = dataset.feature_num
 
@@ -48,11 +50,8 @@ def run(
 
             rewards = []
             for action in actions:
-                reward, done, assets = trading_env.step(action.item())
+                reward, assets = trading_env.step(action.item())
                 rewards.append(reward)
-
-                if done:
-                    break
 
             returns = []
             G = 0
@@ -78,10 +77,42 @@ def run(
             f"Epoch {epoch + 1}/{epochs}, Loss: {loss.item():.4f}, Earnings: {(assets - 1) * 100:.2f}%"
         )
 
+        draw(dataset, trading_env.actions)
+
     if not os.path.exists("output"):
         os.mkdir("output")
     torch.save(model.state_dict, f"output/btcusdt_{interval}_{start}.pth")
 
 
+def draw(dataset, actions):
+    actions_slice = actions
+    data_slice = dataset.data["Close"][dataset.seq_len :].values
+    buy_points = [
+        (i, data_slice[i]) for i in range(len(actions_slice)) if actions_slice[i] == 0
+    ]
+    sell_points = [
+        (i, data_slice[i]) for i in range(len(actions_slice)) if actions_slice[i] == 1
+    ]
+    plt.figure()
+    plt.plot(data_slice, label="price", zorder=1)
+    if buy_points:
+        buy_x, buy_y = zip(*buy_points)
+        plt.scatter(
+            buy_x, buy_y, color="green", label="Buy", marker="^", s=100, zorder=2
+        )
+    if sell_points:
+        sell_x, sell_y = zip(*sell_points)
+        plt.scatter(
+            sell_x, sell_y, color="red", label="Sell", marker="v", s=100, zorder=2
+        )
+    plt.legend()
+    plt.show()
+
+
 if __name__ == "__main__":
-    run(epochs=2, batch_size=32, seq_len=7)
+    run(
+        epochs=10,
+        batch_size=32,
+        seq_len=7,
+        features=["Open", "High", "Low", "Close", "Volume"],
+    )
