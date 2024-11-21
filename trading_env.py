b0843a7df954dd2c2ea9cdf4338cbc641ea7ba65
L-ING <hlf01@icloud.com>
2024-11-21 21:41:56 +0800

    fix: assets calc wrong, improve reward

--- trading_env.py
+++ trading_env.py
@@ -1,23 +1,20 @@
 class TradingEnv:
     def __init__(self, data, transaction_cost=0.001):
         self.data = data
-        self.current_step = 0
         self.transaction_cost = transaction_cost
-        self.position = 0
-        self.entry_price = 0
-        self.assets = 1
-        self.assets_last = 1
+        self.reset()
 
     def reset(self):
         self.current_step = 0
         self.position = 0
         self.entry_price = 0
         self.assets = 1
-        self.assets_last = 1
+        self.assets_initial = self.assets
+        self.assets_last = self.assets
+        self.actions = []
 
     def step(self, action):
-        # action - 0: buy 1: hold 2: sell
-        done = False
+        # action - 0: buy 1: sell 2: hold
         reward = 0
 
         current_price = self.data.iloc[self.current_step]
@@ -25,37 +22,51 @@ class TradingEnv:
         if action == 0:
             if self.position == 0:
                 # buy only when not in position
+                self.actions.append(action)
+
                 self.position = 1
                 self.entry_price = current_price
-                self.assets = (1 - self.transaction_cost) * current_price
+                self.assets = (1 - self.transaction_cost) * self.assets
                 reward = 0
             else:
+                self.actions.append(-1)
                 reward = -1
 
         elif action == 1:
             if self.position == 1:
                 # sell only when in position
-                self.assets = (
-                    self.assets
-                    / self.entry_price
-                    * current_price
-                    * (1 - self.transaction_cost)
-                )
-                reward = (self.assets - self.assets_last) / self.assets_last
+                self.actions.append(action)
+
+                self._update_assets()
+
+                total_profit_rate = (
+                    self.assets - self.assets_initial
+                ) / self.assets_initial
+                this_profit_rate = (self.assets - self.assets_last) / self.assets_last
+                reward = total_profit_rate + this_profit_rate
 
                 self.position = 0
                 self.entry_price = 0
+                self.assets_last = self.assets
             else:
+                self.actions.append(-1)
                 reward = -1
 
         elif action == 2:
+            self.actions.append(action)
+
             if self.position == 1:
                 reward = (current_price - self.entry_price) / self.entry_price
             else:
                 reward = 0
 
         self.current_step += 1
-        if self.current_step >= len(self.data):
-            done = True
 
-        return reward, done, self.assets
+        return reward, self.assets
+
+    def _update_assets(self):
+        current_price = self.data.iloc[self.current_step]
+
+        self.assets = (
+            self.assets / self.entry_price * current_price * (1 - self.transaction_cost)
+        )
